e374aa322d628c04f936277ae0c731dc8e2f25d1
elizabethvilloria <lizvilloria@gmail.com>
2025-02-10 17:34:44 +0100

    Dashboard wip

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import CalendarComponent from "../components/CalendarComponent";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Notiq Dashboard</h1>
+      <CalendarComponent />
+    </div>
+  );
+};
+
+export default Dashboard;
